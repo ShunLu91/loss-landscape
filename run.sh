baf571792be886f5969f82367f01620fcddf5921
lushun_mac <470651748@qq.com>
2020-08-09 20:24:11 +0800

    pp7

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@ if [ ! -d logdir  ];then
 fi
 export HDF5_USE_FILE_LOCKING=FALSE
 
-#CUDA_VISIBLE_DEVICES=2,3 mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
-#--dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot
+CUDA_VISIBLE_DEVICES=7 mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
+--dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot
 
-CUDA_VISIBLE_DEVICES=3 nohup mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
---dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot > ./logdir/pps3.log  2>&1 &
+#CUDA_VISIBLE_DEVICES=7 nohup mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
+#--dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot > ./logdir/pp7_2d.log  2>&1 &
+#tail -f ./logdir/pp7_2d.log
