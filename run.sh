8cf44e54bb7d2c5617a7302b76d2f852ae7fe90b
lushun_mac <470651748@qq.com>
2020-08-09 20:43:37 +0800

    pp7

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ export OMPI_MCA_opal_cuda_support=true
 
 CUDA_VISIBLE_DEVICES=3,7 nohup mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
 --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot > ./logdir/pp7_2d.log  2>&1 &
-tail -f ./logdir/pp7_2d.log
+#tail -f ./logdir/pp7_2d.log
