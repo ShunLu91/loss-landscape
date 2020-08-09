77f0163b440ff78b1ec0e47b8c4cd588f2715340
lushun_mac <470651748@qq.com>
2020-08-09 20:14:10 +0800

    pp7

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-CUDA_VISIBLE_DEVICES=2,3 mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
---dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot --surf_file pp7
+export CUDA_VISIBLE_DEVICES=2,3
+export HDF5_USE_FILE_LOCKING=FALSE
+mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
+--dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot
