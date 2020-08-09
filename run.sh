023469a3b90be114ce43f4b9403ca84323f332ee
lushun_mac <470651748@qq.com>
2020-08-09 19:36:20 +0800

    pp7

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
+CUDA_VISIBLE_DEVICES=2,3 mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
 --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --plot
