3c957a890096f0fc6a01ce43d504bc7d62d74864
lushun_mac <470651748@qq.com>
2020-08-09 20:29:24 +0800

    pp7

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ if [ ! -d logdir  ];then
   mkdir logdir
 fi
 export HDF5_USE_FILE_LOCKING=FALSE
-
+export OMPI_MCA_opal_cuda_support=true
 CUDA_VISIBLE_DEVICES=7 mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7 --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_best.pt \
 --dir_type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot
 
