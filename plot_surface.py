c746866e28b016eab2cd9db19bd12b7399407a34
lushun_mac <470651748@qq.com>
2020-08-10 12:15:49 +0800

    pp7_val email

--- plot_surface.py
+++ plot_surface.py
@@ -24,6 +24,54 @@ import model_loader
 import scheduler
 import mpi4pytorch as mpi
 
+def email_sender(result, config):
+    import smtplib
+    import getpass
+    from email.mime.text import MIMEText
+
+    # 设置服务器所需信息
+    # 163邮箱服务器地址
+    mail_host = 'smtp.163.com'
+    # 163用户名
+    mail_user = '13220102191'
+    # 密码(部分邮箱为授权码)
+    mail_pass = 'KNJNHCRERARMJRXF'
+    # 邮件发送方邮箱地址
+    sender = '13220102191@163.com'
+    # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
+    receivers = ['470651748@qq.com']
+
+    # 设置email信息
+    # 邮件内容设置
+    msg = '/***** \n EXP: {} \n Result: {} \n GPU: {} \n User: {} \n Config: {} \n *****/ \n'.format(
+        config.exp, result, config.gpu, getpass.getuser(), config
+    )
+    message = MIMEText(msg, 'plain', 'utf-8'
+    )
+    # 邮件主题
+    message['Subject'] = 'EXP_{}_Done'.format(config.exp)
+    # 发送方信息
+    message['From'] = sender
+    # 接受方信息
+    message['To'] = receivers[0]
+
+    # 登录并发送邮件
+    try:
+        smtpObj = smtplib.SMTP()
+        # 连接到服务器
+        smtpObj.connect(mail_host, 25)
+        # 登录到服务器
+        smtpObj.login(mail_user, mail_pass)
+        # 发送
+        smtpObj.sendmail(
+            sender, receivers, message.as_string())
+        # 退出
+        smtpObj.quit()
+        print('sending email success')
+    except smtplib.SMTPException as e:
+        print('error', e)  # 打印错误
+
+
 def name_surface_file(args, dir_file):
     # skip if surf_file is specified in args
     if args.surf_file:
@@ -299,3 +347,8 @@ if __name__ == '__main__':
             plot_2D.plot_2d_contour(surf_file, 'test_acc', args.vmin, args.vmax, args.vlevel, args.show)
         else:
             plot_1D.plot_1d_loss_err(surf_file, args.xmin, args.xmax, args.loss_max, args.log, args.show)
+
+    #--------------------------------------------------------------------------
+    # send emails
+    #--------------------------------------------------------------------------
+    email_sender(result='{}-loss-landscape done'.format(args.model), config=args)
