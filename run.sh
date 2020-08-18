f2b334884ec151b32ba93200af0829c4e30a8a01
lushun_mac <470651748@qq.com>
2020-08-18 14:16:26 +0800

    plot_2D

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ export OMPI_MCA_opal_cuda_support=true
 #CUDA_VISIBLE_DEVICES=0,1,2,3 nohup mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7_super --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_super.pth.tar \
 #--dir_type alpha --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot --exp pp7_super --gpu 0,1,2,3 > ./logdir/pp7_super.log  2>&1 &
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 nohup mpirun -n 4 python plot_surface.py --mpi --cuda --model pp7_super --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_super.pth.tar \
+CUDA_VISIBLE_DEVICES=0,1,2,3 nohup mpirun -n 4 python plot_surface.py --mpi --cuda --ngpu 4 --model pp7_super --x=-1:1:51 --y=-1:1:51 --model_file snapshots/pp7_super.pth.tar \
 --dir_type alpha --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn --plot --exp pp7_super2 --gpu 0,1,2,3 > ./logdir/pp7_super2.log  2>&1 &
 
 #tail -f ./logdir/pp7_2d.log
