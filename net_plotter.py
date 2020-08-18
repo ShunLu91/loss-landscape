e6d2e853b64eeebbf3b5576af2f4238379abb834
lushun_mac <470651748@qq.com>
2020-08-18 13:55:45 +0800

    pp7_super2

--- net_plotter.py
+++ net_plotter.py
@@ -60,6 +60,9 @@ def set_alphas(net, weights, directions=None, step=None):
             changes = [d*step for d in directions[0]]
 
         for (p, w, d) in zip(net._arch_parameters, weights, changes):
+            print('p:', p.device)
+            print('w:', p.device)
+            print('d:', p.device)
             p.data = w.data + torch.Tensor(d).type(type(w.data))
 
 
